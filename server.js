6989d4efb03b368e77672545f8bbfad0e5f2d0a5
Brian K <sekulumebrian42@gmail.com>
2025-03-10 05:18:42 +0300

    atlas db connection

--- server.js
+++ server.js
@@ -15,13 +15,38 @@ app.use(express.json()); // Parse JSON requests
 app.use(cors()); // Enable CORS
 
 // MongoDB Connection
+// mongoose
+//   .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/NectarDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log(" MongoDB connected successfully"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
+
+
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+// Connecting to the database
+require("dotenv").config();
+console.log("MONGO_URL:", process.env.MONGO_URI); // Debugging line
+
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/NectarDB", {
+  .connect("mongodb+srv://Brian:Brian@cluster0.0kxi8.mongodb.net/database-1?retryWrites=true&w=majority&appName=Cluster0", {
+   
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log(" MongoDB connected successfully"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+require("dotenv").config();
+const port = process.env.PORT || 3001;
+const MONGO_URI = process.env.MONGO_URI;
+
+
 
 // Routes
 app.use("/signup", signupRoutes); // Ensure signupRoutes is correctly exported
@@ -34,5 +59,5 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
